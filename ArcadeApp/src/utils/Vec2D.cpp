f7d208ef6fd40b2d52ac7b0e7ae962927ffa5c22
Ratio <31981754+ReneDraws@users.noreply.github.com>
2023-05-12 23:16:25 +0200

    Added last functions for the VectorUtilClass. Finished it for now.

--- ArcadeApp/src/utils/Vec2D.cpp
+++ ArcadeApp/src/utils/Vec2D.cpp
@@ -2,6 +2,8 @@
 #include <assert.h>
 #include "Utils.h"
 
+const Vec2D Vec2D::Zero;
+
 // Overloaded console output for a Vector2D
 std::ostream& operator<<(std::ostream & consoleOut, const Vec2D& vec)
 {
@@ -58,4 +60,127 @@ Vec2D& Vec2D::operator/=(float scale)
     
     *this = *this / scale;
     return *this;
+}
+
+// Addition
+Vec2D Vec2D::operator+(const Vec2D& vec) const
+{
+    return Vec2D(mX + vec.mX, mY + vec.mY);
+}
+
+// Subtraction
+Vec2D Vec2D::operator-(const Vec2D& vec) const
+{
+    return Vec2D(mX - vec.mX, mY - vec.mY);
+}
+
+// Addition +=
+Vec2D& Vec2D::operator+=(const Vec2D& vec)
+{
+    *this = *this + vec;
+    return *this;
+}
+
+// Subtraction -=
+Vec2D& Vec2D::operator-=(const Vec2D& vec)
+{
+    *this = *this - vec;
+    return *this;
+}
+
+// Magnitude
+float Vec2D::magnitude() const
+{
+    return sqrt(magnitude2());
+}
+
+float Vec2D::magnitude2() const
+{
+    return mX * mX + mY * mY;
+}
+
+// Normalize
+Vec2D Vec2D::getUnitVec() const
+{
+    float mag = magnitude();
+
+    if(mag > EPSILON) { return *this / mag; }
+    else { return Vec2D::Zero; }
+}
+
+Vec2D& Vec2D::normalize()
+{
+    float mag = magnitude();
+
+    if(mag > EPSILON) { *this /= mag; }
+
+    return *this;
+}
+
+// Distance
+float Vec2D::distance(const Vec2D& vec) const
+{
+    return (vec - *this).magnitude();
+}
+
+// Dot-product of two Vectors
+float Vec2D::dot(const Vec2D& vec) const
+{
+    return (mX * vec.mX) + (mY * vec.mY);    
+}
+
+// Projection
+Vec2D Vec2D::projectOnto(const Vec2D& vec) const
+{
+    Vec2D unitVec2 = vec.getUnitVec();
+
+    float vecdot = dot(unitVec2);
+
+    return unitVec2 * vecdot;
+}
+
+// Angle between vectors
+float Vec2D::angleBetween(const Vec2D& vec) const
+{
+    return acosf(getUnitVec().dot(vec.getUnitVec()));
+}
+
+// Reflect the Vector of this "normal"
+Vec2D Vec2D::reflect(const Vec2D& normal) const
+{
+    // v - 2(v dot n)n
+    return *this - 2 * projectOnto(normal);
+}
+
+// Vector rotation
+void Vec2D::rotate(float angle, const Vec2D& aroundPoint)
+{
+    float cosine = cosf(angle);
+    float sine = sinf(angle);
+    Vec2D thisVec(mX,mY);
+
+    thisVec -= aroundPoint;
+
+    float xRotation = thisVec.mX * cosine - thisVec.mY * sine;
+    float yRoation = thisVec.mX * sine + thisVec.mY * cosine;
+
+    Vec2D rot = Vec2D(xRotation,yRoation);
+    
+    *this = rot + aroundPoint;
+}
+
+Vec2D Vec2D::rotationResult(float angle, const Vec2D& aroundPoint) const
+{
+    float cosine = cosf(angle);
+    float sine = sinf(angle);
+    Vec2D thisVec(mX,mY);
+
+    thisVec -= aroundPoint;
+
+    float xRotation = thisVec.mX * cosine - thisVec.mY * sine;
+    float yRoation = thisVec.mX * sine + thisVec.mY * cosine;
+
+    Vec2D rot = Vec2D(xRotation,yRoation);
+
+    return rot + aroundPoint;
 }
--- ArcadeApp/src/utils/Vec2D.h
+++ ArcadeApp/src/utils/Vec2D.h
@@ -4,7 +4,10 @@
 // 2D-Coordinate class
 class Vec2D {
 public:
+    
+    
     // Constructor
+    static const Vec2D Zero;                        // Zero vector
     Vec2D() : mX(0), mY(0) {}
     Vec2D(float x, float y) : mX(x), mY(y) {}
     
@@ -24,10 +27,31 @@ public:
     Vec2D operator/(float scale) const;             // Divide by a scalar
     Vec2D& operator*=(float scale);                 // Multiply and add
     Vec2D& operator/=(float scale);                 // Divide and add
+    Vec2D operator+(const Vec2D& vec) const;        // Addition
+    Vec2D operator-(const Vec2D& vec) const;        // Subtraction
+    Vec2D& operator+=(const Vec2D& vec);            // Add
+    Vec2D& operator-=(const Vec2D& vec);            // Subtract and add
 
+    
+    float magnitude() const;                        // Squareroot of Magnitude 
+    float magnitude2() const;                       // Magnitude
+
+    Vec2D getUnitVec() const;
+    Vec2D& normalize();
+
+    float distance(const Vec2D& vec) const;         // Distance
+    float dot(const Vec2D& vec) const;              // Dot-Product
+
+    Vec2D projectOnto(const Vec2D& vec) const;
+
+    float angleBetween(const Vec2D& vec) const;
+    Vec2D reflect(const Vec2D& normal) const;       // Reflect the Vector of this "normal"
+
+    void rotate(float angle, const Vec2D& aroundPoint);
+    Vec2D rotationResult(float angle, const Vec2D& aroundPoint) const;
+    
     friend Vec2D operator*(float scale, const Vec2D& vec);
-    
-    
+
 private:
     float mX,mY;
 };
